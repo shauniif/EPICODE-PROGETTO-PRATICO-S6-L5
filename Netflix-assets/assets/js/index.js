44cb6f4cd13fff5fb06b0964af14ab429247952a
shauniif <francescavescio12@gmail.com>
2024-05-05 20:26:17 +0200

    -

--- Netflix-assets/assets/js/index.js
+++ Netflix-assets/assets/js/index.js
@@ -5,31 +5,14 @@
     yearsSpan.innerText = currentYear;
     };
 
-// PROFILE CHECKED
-    if(document.getElementById('boxchecked1')) {
-    const checkedbutton1 = document.getElementById('boxchecked1');
-    const checkedIcon1 = document.getElementById('checked1');
-    checkedbutton1.addEventListener('click', function() {
-    checkedIcon1.classList.toggle('visibility');
-        })
-    };
-    if(document.getElementById('boxchecked2')) {
-        const checkedbutton2 = document.getElementById('boxchecked2');
-        const checkedIcon2 = document.getElementById('checked2');
-        checkedbutton2.addEventListener('click', function() {
-        checkedIcon2.classList.toggle('visibility');
-        })
-    }
-
     if(document.getElementsByClassName('img-fluid')) {
         const imgScale = document.getElementsByClassName('img-fluid');
         console.log(imgScale);
         const imgScaleArr = Array.from(imgScale);
-        if(imgScaleArr.length > 1) {
             for(i=0; i < imgScaleArr.length; i++) {
             imgScale[i].classList.add('scaleimg');
             } 
-        }
+        
     } 
 
 
